a1264f9437ddd4d21d7645663cf35a94a6498159
Baizey <Baizey@users.noreply.github.com>
2020-09-09 18:59:12 +0200

    correct such that populations always contains individuals

--- EvolutionaryAlgorithm.Core/IPopulation.cs
+++ EvolutionaryAlgorithm.Core/IPopulation.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using EvolutionaryAlgorithm.Core.Individual;
 
 namespace EvolutionaryAlgorithm.Core
 {
-    public interface IPopulation<T> : IEnumerable<T>, ICloneable
+    public interface IPopulation<T> : IEnumerable<IIndividual<T>>, ICloneable
         where T : ICloneable
     {
-        public List<T> Individuals { get; set; }
+        public List<IIndividual<T>> Individuals { get; set; }
         public int Generation { get; set; }
 
-        public T this[int i]
+        public IIndividual<T> this[int i]
         {
             get => Individuals[i];
             set => Individuals[i] = value;
--- EvolutionaryAlgorithm.Templates/Population/Population.cs
+++ EvolutionaryAlgorithm.Templates/Population/Population.cs
@@ -3,27 +3,29 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using EvolutionaryAlgorithm.Core;
+using EvolutionaryAlgorithm.Core.Individual;
 
 namespace EvolutionaryAlgorithm.Template.Population
 {
     public class Population<T> : IPopulation<T> where T : ICloneable
     {
-        public List<T> Individuals { get; set; }
+        public List<IIndividual<T>> Individuals { get; set; }
         public int Generation { get; set; } = 0;
 
-        public Population(List<T> value) => Individuals = value;
+        public Population(List<IIndividual<T>> value) => Individuals = value;
 
-        public Population(int size, Func<T> generator) =>
+        public Population(int size, Func<IIndividual<T>> generator) =>
             Individuals = Enumerable.Range(0, size).Select(_ => generator.Invoke()).ToList();
 
-        public IEnumerator<T> GetEnumerator()
+        public IEnumerator<IIndividual<T>> GetEnumerator()
         {
             return Individuals.GetEnumerator();
         }
 
         public override string ToString() => Individuals.ToString();
 
-        public object Clone() => new Population<T>(Individuals.Select(individual => (T) individual.Clone()).ToList());
+        public object Clone() =>
+            new Population<T>(Individuals.Select(individual => (IIndividual<T>) individual.Clone()).ToList());
 
         IEnumerator IEnumerable.GetEnumerator()
         {

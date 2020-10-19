da1bc9ebccedbc04af906ecc56cd4f333fcd6fdb
Baizey <Baizey@users.noreply.github.com>
2020-10-19 18:04:45 +0200

    move all mutation-rate related handling to mutation

--- EvolutionaryAlgorithm.Templates/Endogenous/EndogenousGenerationGenerator.cs
+++ EvolutionaryAlgorithm.Templates/Endogenous/EndogenousGenerationGenerator.cs
@@ -9,30 +9,12 @@ namespace EvolutionaryAlgorithm.Template.Endogenous
 {
     public class EndogenousGenerationGenerator : BitGenerationGenerator<IEndogenousBitIndividual>
     {
-        private readonly int _learningRate;
-        private int _maxRate;
-
         public EndogenousGenerationGenerator(int learningRate)
         {
             Mutator = new Mutator<IEndogenousBitIndividual, BitArray, bool>()
                 .CloneGenesFrom(new FirstParentSelector<IEndogenousBitIndividual>())
                 .ThenApply(new EndogenousMutation(learningRate));
             Filter = new EndogenousGenerationFilter();
-            _learningRate = learningRate;
-        }
-
-        public override void Initialize()
-        {
-            var g = Algorithm.Parameters.GeneCount;
-            _maxRate = (int) Math.Pow(_learningRate, Math.Log(g / (2D * _learningRate), _learningRate));
-            base.Initialize();
-        }
-
-        public override void Update()
-        {
-            var individual = Algorithm.Population[0];
-            var r = individual.MutationRate;
-            individual.MutationRate = Math.Min(Math.Max(_learningRate, r), _maxRate);
         }
     }
 }
--- EvolutionaryAlgorithm.Templates/Endogenous/EndogenousMutation.cs
+++ EvolutionaryAlgorithm.Templates/Endogenous/EndogenousMutation.cs
@@ -13,12 +13,21 @@ namespace EvolutionaryAlgorithm.Template.Endogenous
         private readonly MutationApplier _applier = new MutationApplier();
         private readonly int _learningRate;
         private readonly Random _random = new Random();
-        private int _geneCount;
+        private int _geneCount, _maxRate, _minRate;
         public IEvolutionaryAlgorithm<IEndogenousBitIndividual, BitArray, bool> Algorithm { get; set; }
 
         public EndogenousMutation(int learningRate) => _learningRate = learningRate;
 
-        public void Initialize() => _geneCount = Algorithm.Parameters.GeneCount;
+        public void Initialize()
+        {
+            _geneCount = Algorithm.Parameters.GeneCount;
+            _maxRate = (int) Math.Pow(
+                _learningRate,
+                Math.Log(
+                    _geneCount / (2D * _learningRate),
+                    _learningRate));
+            _minRate = 1;
+        }
 
         public void Update()
         {
@@ -26,10 +35,10 @@ namespace EvolutionaryAlgorithm.Template.Endogenous
 
         public void Mutate(int index, IEndogenousBitIndividual child)
         {
-            if (_random.NextDouble() >= 0.5)
-                child.MutationRate /= _learningRate;
+            if (_random.NextDouble() < 0.5)
+                child.MutationRate = Math.Max(_minRate, (int) child.MutationRate / _learningRate);
             else
-                child.MutationRate *= _learningRate;
+                child.MutationRate = Math.Min(_maxRate, (int) child.MutationRate * _learningRate);
             _applier.Mutate(child, child.MutationRate, _geneCount);
         }
     }

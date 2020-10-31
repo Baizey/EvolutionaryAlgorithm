b56fd2d344bcc5aa091b2737bed04e4240286889
Baizey <Baizey@users.noreply.github.com>
2020-10-31 20:33:42 +0100

    add basic graph randomize constructor

---
+++ EvolutionaryAlgorithm.Templates/Graph/Edge.cs
@@ -0,0 +1,20 @@
+﻿namespace EvolutionaryAlgorithm.Template.Graph
+{
+    public class Edge
+    {
+        private static int _nextId;
+        public static void Reset() => _nextId = 0;
+        public Node From { get; }
+        public Node To { get; }
+        public int Id { get; }
+        public double Distance { get; }
+
+        public Edge(Node from, Node to)
+        {
+            Id = _nextId++;
+            From = from;
+            To = to;
+            Distance = From.Distance(To);
+        }
+    }
+}
---
+++ EvolutionaryAlgorithm.Templates/Graph/Node.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EvolutionaryAlgorithm.Template.Graph
+{
+    public class Node
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public List<Edge> Edges { get; } = new List<Edge>();
+
+        public void Add(Node other, Graph graph = null)
+        {
+            var edge = new Edge(this, other);
+            Edges.Add(edge);
+            other.Edges.Add(edge);
+            graph?.Edges.Add(edge);
+        }
+
+        public Node()
+        {
+        }
+
+        public Node(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double Distance(Node other) => Math.Abs(X - other.X) + Math.Abs(Y - other.Y);
+    }
+}

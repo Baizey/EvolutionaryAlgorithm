a896a66f11e888acb82afe3cc6d445b393a79d6b
Lasse Spindler Mørk <Baizey@users.noreply.github.com>
2020-11-16 10:00:57 +0100

    update benchmark

--- EvolutionaryAlgorithm/Benchmark.cs
+++ EvolutionaryAlgorithm/Benchmark.cs
@@ -1,19 +1,114 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Drawing;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using EvolutionaryAlgorithm.BitImplementation;
 using EvolutionaryAlgorithm.Core.Algorithm;
+using EvolutionaryAlgorithm.Core.HyperHeuristic;
+using EvolutionaryAlgorithm.Core.Parameters;
 using EvolutionaryAlgorithm.Core.Terminations;
+using EvolutionaryAlgorithm.GUI.Models.Enums;
+using EvolutionaryAlgorithm.Template;
+using EvolutionaryAlgorithm.Template.FitnessFunctions;
 
 namespace EvolutionaryAlgorithm
 {
     public static class Benchmark
     {
-        public static async Task Range(
+        public static async Task RunBenchmark(
+            FitnessFunctions? fitness = null,
+            Heuristics? heuristic = null,
+            int geneStep = 100,
+            int mutationRate = 2,
+            double learningRate = 2,
+            int? mu = null,
+            int? lambda = null,
+            int? observationPhase = null,
+            double? repairChance = null,
+            double? beta = null,
+            int? jump = null,
+            int? limitFactor = null)
+        {
+            var filename = $"{heuristic}_{fitness}_{mutationRate}_{learningRate}";
+            if (lambda != null) filename += $"_{lambda}";
+            if (observationPhase != null) filename += $"_{observationPhase}";
+            if (beta != null) filename += $"_{beta}";
+            if (repairChance != null) filename += $"_{repairChance}";
+            if (limitFactor != null) filename += $"_{limitFactor}";
+            if (jump != null) filename += $"_{jump}";
+
+            var range = new List<Func<IEvolutionaryAlgorithm<IBitIndividual, BitArray, bool>>>();
+            var toCords = new List<Func<IEvolutionaryAlgorithm<IBitIndividual, BitArray, bool>, Point>>();
+            for (var i = 0; i < 20; i++)
+            {
+                var g = (i + 1) * geneStep;
+                range.Add(() => new BitEvolutionaryAlgorithm<IBitIndividual>()
+                    .UsingBasicStatistics()
+                    .UsingRandomPopulation(mutationRate)
+                    .UsingEvaluation(CreateFitness(fitness, jump ?? 5))
+                    .UsingParameters(new Parameters
+                    {
+                        Mu = mu ?? 1,
+                        Lambda = lambda ?? 1,
+                        GeneCount = g,
+                        MutationRate = mutationRate
+                    })
+                    .UsingHeuristic(CreateHeuristic(
+                        heuristic, learningRate, mutationRate, observationPhase ?? 10,
+                        repairChance ?? 1, beta ?? 1.5D, limitFactor ?? 1)));
+
+                toCords.Add(algo => new Point(g, (int) algo.Statistics.Generations));
+            }
+
+            await Benchmark.Range(filename, range, toCords);
+        }
+
+        private static IHyperHeuristic<IBitIndividual, BitArray, bool> CreateHeuristic(Heuristics? heuristic,
+            double learningRate = 0.05D,
+            int mutationRate = 2,
+            int observationPhase = 10,
+            double repairChance = 1,
+            double beta = 1.5,
+            int limitFactor = 1)
+        {
+            return heuristic switch
+            {
+                Heuristics.Asymmetric => new SimpleHeuristic<IBitIndividual, BitArray, bool>(
+                    PresetGenerator.Asymmetric(learningRate, observationPhase)),
+                Heuristics.Repair => new SimpleHeuristic<IBitIndividual, BitArray, bool>(
+                    PresetGenerator.Repair((int) learningRate, repairChance)),
+                Heuristics.SingleEndogenous => new SimpleHeuristic<IBitIndividual, BitArray, bool>(
+                    PresetGenerator.SingleEndogenous((int) learningRate)),
+                Heuristics.MultiEndogenous => new SimpleHeuristic<IBitIndividual, BitArray, bool>(
+                    PresetGenerator.MultiEndogenous((int) learningRate)),
+                Heuristics.HeavyTail => new SimpleHeuristic<IBitIndividual, BitArray, bool>(
+                    PresetGenerator.HeavyTail((int) learningRate, beta)),
+                Heuristics.StagnationDetection => PresetGenerator.StagnationDetection(mutationRate, limitFactor,
+                    (int) learningRate),
+                _ => throw new InvalidEnumArgumentException()
+            };
+        }
+
+        private static IBitFitness<IBitIndividual> CreateFitness(
+            FitnessFunctions? fitnessFunction,
+            int jump = 0)
+        {
+            return fitnessFunction switch
+            {
+                FitnessFunctions.OneMax => new OneMaxFitness<IBitIndividual>(),
+                FitnessFunctions.Jump => new JumpFitness<IBitIndividual>(jump),
+                FitnessFunctions.LeadingOnes => new LeadingOnesFitness<IBitIndividual>(),
+                FitnessFunctions.MinimumSpanningTree => throw new NotImplementedException(),
+                FitnessFunctions.Satisfiability => throw new NotImplementedException(),
+                _ => throw new InvalidEnumArgumentException()
+            };
+        }
+
+        private static async Task Range(
             string filename,
             List<Func<IEvolutionaryAlgorithm<IBitIndividual, BitArray, bool>>> generator,
             List<Func<IEvolutionaryAlgorithm<IBitIndividual, BitArray, bool>, Point>> toCords,
@@ -29,7 +124,7 @@ namespace EvolutionaryAlgorithm
                 var tasks = new List<Task>();
                 var algorithms = new List<IEvolutionaryAlgorithm<IBitIndividual, BitArray, bool>>();
                 var toCordsUnrolled = new List<Func<IEvolutionaryAlgorithm<IBitIndividual, BitArray, bool>, Point>>();
-                
+
                 for (var i = 0; i < rounds; i++)
                 {
                     var algo = generator[j].Invoke();
@@ -48,15 +143,17 @@ namespace EvolutionaryAlgorithm
                                     Console.WriteLine($"Progress: {c} / {total} ({progress}%) ~{remaining} remaining");
                             }));
                 }
-                
+
                 await Task.WhenAll(tasks);
                 for (var i = 0; i < algorithms.Count; i++)
                 {
                     var point = toCordsUnrolled[i].Invoke(algorithms[i]);
                     await file.WriteLineAsync($"{point.X} {point.Y}");
                 }
+
                 await file.FlushAsync();
             }
+
             Console.WriteLine("Done...");
         }
     }
--- EvolutionaryAlgorithm/Program.cs
+++ EvolutionaryAlgorithm/Program.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Drawing;
+﻿using System.ComponentModel;
 using System.Threading.Tasks;
-using EvolutionaryAlgorithm.BitImplementation;
-using EvolutionaryAlgorithm.Core.Algorithm;
-using EvolutionaryAlgorithm.Core.Parameters;
-using EvolutionaryAlgorithm.Template;
-using EvolutionaryAlgorithm.Template.FitnessFunctions;
-using static EvolutionaryAlgorithm.Template.PresetGenerator;
+using static EvolutionaryAlgorithm.Benchmark;
+using static EvolutionaryAlgorithm.GUI.Models.Enums.FitnessFunctions;
+using static EvolutionaryAlgorithm.GUI.Models.Enums.Heuristics;
 
 namespace EvolutionaryAlgorithm
 {
@@ -16,49 +10,141 @@ namespace EvolutionaryAlgorithm
     {
         public static async Task Main(string[] args)
         {
-            var algorithm = args[0];
-            var mutationRate = double.Parse(args[1]);
-            var learningRate = double.Parse(args[2]);
-            var extra = double.Parse(args[3]);
-            switch (algorithm)
+            var mode = int.Parse(args[0]);
+            switch (mode)
             {
-                case "Asymmetric":
-                    await AsymmetricStatic(
-                        $"Asymmetric_{mutationRate}_{learningRate}_{extra}",
-                        (int) mutationRate,
-                        learningRate,
-                        (int) extra);
+                case 1:
+                    await RunBenchmark(heuristic: MultiEndogenous, fitness: OneMax, geneStep: 1000, mutationRate: 2,
+                        mu: 10,
+                        lambda: 10,
+                        learningRate: 2);
                     break;
-                default: throw new ArgumentException(algorithm);
+                case 2:
+                    await RunBenchmark(heuristic: MultiEndogenous, fitness: OneMax, geneStep: 1000, mutationRate: 2,
+                        mu: 100,
+                        lambda: 100,
+                        learningRate: 2);
+                    break;
+                case 3:
+                    await RunBenchmark(heuristic: MultiEndogenous, fitness: OneMax, geneStep: 1000, mutationRate: 2,
+                        mu: 50,
+                        lambda: 100,
+                        learningRate: 2);
+                    break;
+                case 4:
+                    await RunBenchmark(heuristic: MultiEndogenous, fitness: LeadingOnes, geneStep: 100, mutationRate: 2,
+                        mu: 10,
+                        lambda: 10,
+                        learningRate: 2);
+                    break;
+                case 5:
+                    await RunBenchmark(heuristic: MultiEndogenous, fitness: LeadingOnes, geneStep: 100, mutationRate: 2,
+                        mu: 100,
+                        lambda: 100,
+                        learningRate: 2);
+                    break;
+                case 6:
+                    await RunBenchmark(heuristic: MultiEndogenous, fitness: Jump, geneStep: 10, mutationRate: 2,
+                        mu: 100,
+                        lambda: 100,
+                        learningRate: 4,
+                        jump: 5);
+                    break;
+                case 7:
+                    await RunBenchmark(heuristic: MultiEndogenous, fitness: Jump, geneStep: 10, mutationRate: 2,
+                        mu: 100,
+                        lambda: 100,
+                        learningRate: 5,
+                        jump: 5);
+                    break;
+                case 8:
+                    await RunBenchmark(heuristic: MultiEndogenous, fitness: Jump, geneStep: 10, mutationRate: 2,
+                        mu: 100,
+                        lambda: 100,
+                        learningRate: 6,
+                        jump: 5);
+                    break;
+                case 9:
+                    await RunBenchmark(heuristic: SingleEndogenous, fitness: OneMax, mu: 1, geneStep: 1000,
+                        mutationRate: 2,
+                        lambda: 10,
+                        learningRate: 2);
+                    break;
+                case 10:
+                    await RunBenchmark(heuristic: SingleEndogenous, fitness: OneMax, mu: 1, geneStep: 1000,
+                        mutationRate: 2,
+                        lambda: 50,
+                        learningRate: 2);
+                    break;
+                case 11:
+                    await RunBenchmark(heuristic: SingleEndogenous, fitness: OneMax, mu: 1, geneStep: 1000,
+                        mutationRate: 2,
+                        lambda: 100,
+                        learningRate: 2);
+                    break;
+                case 12:
+                    await RunBenchmark(heuristic: SingleEndogenous, fitness: LeadingOnes, mu: 1, geneStep: 100,
+                        mutationRate: 2,
+                        lambda: 100,
+                        learningRate: 2);
+                    break;
+                case 13:
+                    await RunBenchmark(heuristic: SingleEndogenous, fitness: LeadingOnes, mu: 1, geneStep: 100,
+                        mutationRate: 2,
+                        lambda: 1000,
+                        learningRate: 2);
+                    break;
+                case 14:
+                    await RunBenchmark(heuristic: SingleEndogenous, fitness: Jump, mu: 1, geneStep: 100,
+                        mutationRate: 2,
+                        lambda: 100,
+                        learningRate: 5,
+                        jump: 5);
+                    break;
+                case 15:
+                    await RunBenchmark(heuristic: SingleEndogenous, fitness: Jump, mu: 1, geneStep: 100,
+                        mutationRate: 2,
+                        lambda: 100,
+                        learningRate: 4,
+                        jump: 5);
+                    break;
+                case 16:
+                    await RunBenchmark(heuristic: SingleEndogenous, fitness: Jump, mu: 1, geneStep: 100,
+                        mutationRate: 2,
+                        lambda: 100,
+                        learningRate: 6,
+                        jump: 5);
+                    break;
+                case 17:
+                    await RunBenchmark(heuristic: Asymmetric, fitness: OneMax, mu: 1, lambda: 1, geneStep: 1000,
+                        mutationRate: 1,
+                        learningRate: 0.1,
+                        observationPhase: 50);
+                    break;
+                case 18:
+                    await RunBenchmark(heuristic: Asymmetric, fitness: Jump, mu: 1, lambda: 1, geneStep: 1000,
+                        jump: 4,
+                        mutationRate: 2,
+                        learningRate: 0.02,
+                        observationPhase: 10);
+                    break;
+                case 19:
+                    await RunBenchmark(heuristic: Asymmetric, fitness: Jump, mu: 1, lambda: 1, geneStep: 1000,
+                        jump: 4,
+                        mutationRate: 4,
+                        learningRate: 0.02,
+                        observationPhase: 10);
+                    break;
+                case 20:
+                    await RunBenchmark(heuristic: Asymmetric, fitness: Jump, mu: 1, lambda: 1, geneStep: 1000,
+                        jump: 4,
+                        mutationRate: 8,
+                        learningRate: 0.02,
+                        observationPhase: 10);
+                    break;
+                default:
+                    throw new InvalidEnumArgumentException();
             }
-        }
-
-        public static async Task AsymmetricStatic(string filename,
-            int mutationRate,
-            double learningRate,
-            int observationPhase)
-        {
-            var range = new List<Func<IEvolutionaryAlgorithm<IBitIndividual, BitArray, bool>>>();
-            var toCords = new List<Func<IEvolutionaryAlgorithm<IBitIndividual, BitArray, bool>, Point>>();
-            for (var i = 0; i < 20; i++)
-            {
-                var g = (i + 1) * 1000;
-                range.Add(() => new BitEvolutionaryAlgorithm<IBitIndividual>()
-                    .UsingBasicStatistics()
-                    .UsingRandomPopulation()
-                    .UsingEvaluation(new LeadingOnesFitness<IBitIndividual>())
-                    .UsingHeuristic(Asymmetric(learningRate, observationPhase))
-                    .UsingParameters(new Parameters
-                    {
-                        GeneCount = g,
-                        Lambda = 1,
-                        Mu = 1,
-                        MutationRate = mutationRate
-                    }));
-                toCords.Add(algo => new Point(g, (int) algo.Statistics.Generations));
-            }
-
-            await Benchmark.Range(filename, range, toCords);
         }
     }
 }
